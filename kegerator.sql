d16f4b0eb43135d5bb8262f1b6f4042f559d81a3
dcarney <dcarney@gmail.com>
2011-03-16 00:44:55 -0700

    WIP. Modified the DB schema slightly, to allow for setting an "active" keg.  Added some more queries to the keg.db object, and made the keg.io object keep track of the last RFID tag it's seen.  This comes in useful when you need to "pour" on subsequent events from the arduino.

--- kegerator.sql
+++ kegerator.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user(first_name varchar(64), last_name varchar(64), rfid varchar(10));
-CREATE TABLE keg(keg_id int, beer varchar(64), brewery varchar(64), description varchar(256), tapped_date datetime, killed_date datetime);
+CREATE TABLE keg(keg_id int, beer varchar(64), brewery varchar(64), description varchar(256), tapped_date datetime, killed_date datetime, active boolean);
 CREATE TABLE pour(rfid varchar(10), keg_id int, pour_date date, volume_ounces int);
 CREATE TABLE temperature(temperature int, temperature_date date);
 
@@ -7,8 +7,8 @@ INSERT INTO user(rfid, first_name, last_name) VALUES('2312A4B540', 'Dylan', 'Car
 INSERT INTO user(rfid, first_name, last_name) VALUES('2312A4B541', 'Chris', 'Castle');
 INSERT INTO user(rfid, first_name, last_name) VALUES('2312A4B542', 'Carl', 'Krauss');
 
-INSERT INTO keg(keg_id, beer, brewery, description, tapped_date) 
-VALUES(1, 'Mannys Pale Ale', 'Georgetown Brewery', 'A solid pale ale, brewed in Seattle least-douchey neighborhood.', '2011-03-12 01:23:45.666');
+INSERT INTO keg(keg_id, beer, brewery, description, tapped_date, active) 
+VALUES(1, 'Mannys Pale Ale', 'Georgetown Brewery', 'A solid pale ale, brewed in Seattle least-douchey neighborhood.', '2011-03-12 01:23:45.666', 'true');
 
 INSERT INTO temperature(temperature, temperature_date) VALUES (39, '2011-03-12 01:23:46.666');
 INSERT INTO temperature(temperature, temperature_date) VALUES (39, '2011-03-12 01:23:47.666');
