dad4547e83a8e6836aec5ca0fd8a46038d610fcf
Chris Castle <crcastle@gmail.com>
2011-04-09 16:36:47 -0700

    removed express/support/other junk dependencies. they're not needed. added choreographer for routing and node-static for static file serving.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //setup Dependencies
-require(__dirname + "/lib/setup").ext( __dirname + "/lib").ext( __dirname + "/lib/express/support");
-var connect = require('connect')
-	, fs = require('fs')
-    , express = require('express')
+require(__dirname + "/lib/setup").ext( __dirname + "/lib");
+var	  fs = require('fs')
     , sys = require('sys')
+	, http = require('http')
     , io = require('Socket.IO-node')
+	, files = require('node-static')
+	, router = require('choreographer').router()
     , port = (process.env.PORT || 8081)
 	, keg_io = require('keg.io')
 	, keg = new keg_io.Keg();
@@ -15,7 +16,7 @@ var connect = require('connect')
 // keg.init("/dev/cu.usbserial-A400fGxO");
 
 // This is the arduino attached to the kegerator
-keg.init("/dev/cu.usbserial-A400fOlX");
+//keg.init("/dev/cu.usbserial-A400fOlX");
 
 /*  The "protocol" we're using to communicate with the arduino consists of the 
     following messages:
@@ -35,43 +36,75 @@ node --> arduino
 
 */
 
-//Setup Express
-var server = express.createServer();
-server.configure(function(){
-    server.set('views', __dirname + '/views');
-    server.use(connect.bodyDecoder());
-    server.use(connect.staticProvider(__dirname + '/static'));
-    server.use(server.router);
+//
+// Create several node-static server instances to serve the './public' folder
+//
+var base = new(files.Server)('./static');
+var css = new(files.Server)('./static/css');
+var css2 = new(files.Server)('./static/css/ui-lightness');
+var css3 = new(files.Server)('./static/css/ui-lightness/images');
+var images = new(files.Server)('./static/images');
+var js = new(files.Server)('./static/js');
+var js2 = new(files.Server)('./static/js/profiling');
+
+///////////////////////////////////////////
+//              Routes                   //
+///////////////////////////////////////////
+router.ignoreCase = true;	
+/////// ADD ALL YOUR ROUTES HERE  /////////
+router.get('/', function(req, res) {
+	base.serveFile('/index.html', 200, {}, req, res);
+})
+.get('/temperatureHistory.json', function(req, res) {
+	// send static sample data for now
+	/*
+	var result = '{"name":"tempHistory","value":[["2011-03-15 '
+		+'01:29:48.666",66],["2011-03-12 '
+		+'01:23:48.666",39],["2011-03-12 '
+		+'01:23:47.666",39],["2011-03-12 01:23:46.666",39]]}';
+	res.send(result);
+	*/
+		
+	keg.getTemperatureTrend(function(result) {
+		// For some reason, the following line doesn't work with the highcharts.
+		//res.send(result, {'Content-Type': 'text/json'}, 200);
+		//console.log(result);
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end(result);
+	});
+})
+.get('/pourHistory.json', function(req, res) {
+	keg.getPourTrend(function(result) {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.end(result);
+	});
+})
+.get('/css/ui-lightness/*', function(req, res, file) {
+	css2.serveFile(file, 200, {}, req, res);
+})
+.get('/css/ui-lightness/images/*', function(req, res, file) {
+	css3.serveFile(file, 200, {}, req, res);
+})
+.get('/css/*', function(req, res, file) {
+	css.serveFile(file, 200, {}, req, res);
+})
+.get('/images/*', function(req, res, file) {
+	images.serveFile(file, 200, {}, req,res);
+})
+.get('/js/profiling/*', function(req, res, file) {
+	js2.serveFile(file, 200, {}, req, res);
+})
+.get('/js/*', function(req, res, file) {
+	js.serveFile(file, 200, {}, req,res);
 });
 
-//setup the errors
-server.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-        res.render('404.ejs', { locals: { 
-                  header: '#Header#'
-                 ,footer: '#Footer#'
-                 ,title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
-    } else {
-        res.render('500.ejs', { locals: { 
-                  header: '#Header#'
-                 ,footer: '#Footer#'
-                 ,title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
-    }
-});
-server.listen( port);
+// create http server
+var server = http.createServer(router);
+server.listen(port);
 
 //Setup Socket.IO
-var io = io.listen(server);
-io.on('connection', function(client){
+var socket = io.listen(server);
+socket.on('connection', function(client){
 	console.log('Client Connected');
 	
 	keg.on('temp', function(data) {
@@ -109,99 +142,10 @@ io.on('connection', function(client){
 		{
 			client.send(JSON.stringify({name: 'remaining', value: data }));
 		}
-	})
+	});
 	client.on('disconnect', function(){
 		console.log('Client Disconnected.');
 	});
 });
-
-
-///////////////////////////////////////////
-//              Routes                   //
-///////////////////////////////////////////
-
-/////// ADD ALL YOUR ROUTES HERE  /////////
-
-server.get('/', function(req,res){
-	
-  res.render('index.ejs', {
-    locals : { 
-              header: '#Header#'
-             ,footer: '#Footer#'
-             ,title : 'keg.io'
-             ,description: 'Info, status, and statistics about a kegerator. '
-							+ 'Receives kegerator sensor data from an Arduino and displays it on your web browser!'
-             ,author: 'VNC'
-             ,analyticssiteid: 'XXXXXXX'
-			 ,site: {
-				 labelImageUrl: 'images/MannysPint3.gif'
-				,beer: 'Manny\'s Pale Ale' 
-				,description: 'A solid pale ale, brewed in Seattle\'s least-douchey neighborhood'
-				,brewery: 'Georgetown Brewery'
-				,ozRemaining: 235
-				,currentTemp: 43
-				,kegStatus: 'ok'
-			 }
-			 ,current: {
-				lastPour: {
-					 name: 'Dylan Carney'
-					,oz: 16
-				}
-			 }
-			 ,dispense: {
-				currentlyPouring: 'Dylan Carney'
-			 }
-  		}
-	});
-});
-
-
-// Define AJAX routes
-
-// History of the keg's temperature
-server.get('/temperatureHistory.json', function(req, res) {
-	// send static sample data for now
-	/*
-	var result = '{"name":"tempHistory","value":[["2011-03-15 '
-		+'01:29:48.666",66],["2011-03-12 '
-		+'01:23:48.666",39],["2011-03-12 '
-		+'01:23:47.666",39],["2011-03-12 01:23:46.666",39]]}';
-	res.send(result);
-	*/
-		
-	keg.getTemperatureTrend(function(result) {
-		// For some reason, the following line doesn't work with the highcharts.
-		//res.send(result, {'Content-Type': 'text/json'}, 200);
-		console.log(result);
-		res.send(result);
-	});
-	
-});
-
-// History of "pours" by user, for the active keg
-server.get('/pourHistory.json', function(req, res) {
-	
-	keg.getPourTrend(function(result) {
-		res.send(result);
-	});
-	
-});
-
-// A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
-});
-
-//The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
-    throw new NotFound;
-});
-
-function NotFound(msg){
-    this.name = 'NotFound';
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
-}
-
 
 console.log('Listening on http://0.0.0.0:' + port );
