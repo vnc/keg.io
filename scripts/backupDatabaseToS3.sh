f82ef114831aa796262ef36cf0db7a29919e8aca
dcarney <dcarney@gmail.com>
2011-05-19 11:22:25 -0700

    Finished up Issue #3 (Create cron job to back up DB to S3 nightly).  A crontab entry has been created for keg@minibox.

--- scripts/backupDatabaseToS3.sh
+++ scripts/backupDatabaseToS3.sh
@@ -6,6 +6,12 @@
 # assumptions:
 #  - s3cmd is installed and on the path
 #  - s3cmd is configured with the proper credentials for accessing the given bucketname
+#
+# NOTE:
+# to get this script to run properly in cron, absolute paths may need to be given
+# for both s3cmd and it's config file (~/.s3cfg), since cron uses different user env settings
+#
+# ex. /home/keg/local/bin/s3cmd --config /home/keg/.s3cfg blah blah blah
 
 DATE="`date +%d-%b-%Y_%H%M%S`"
 s3cmd put ../db/kegerator.db s3://$1/DBBackups/kegerator.db.$DATE
